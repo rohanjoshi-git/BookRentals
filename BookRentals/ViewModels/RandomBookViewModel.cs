83805acf7f7c9dc86f9ae8d4a935055936d58032
Rohan <Rohan@LAPTOP-2FE2DMGC>
2017-09-25 00:22:48 +1300

    Add project files.

---
+++ BookRentals/ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookRentals.Models;
+
+namespace BookRentals.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
