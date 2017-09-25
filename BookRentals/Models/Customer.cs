bbf5eff465ef5ae718ba94a57003c19b18caf19a
Rohan <Rohan@LAPTOP-2FE2DMGC>
2017-09-25 21:46:22 +1300

    Created Entity Framework using code first approach

--- BookRentals/Models/Customer.cs
+++ BookRentals/Models/Customer.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations; // for [Required]
 
 namespace BookRentals.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } // Navigation Property - Customer to MembershipType
+        public byte MembershipTypeId { get; set; }
     }
 }
