ef87ae557bfb12dd516f41875c26a0fc71a761c8
Rohan <Rohan@LAPTOP-2FE2DMGC>
2017-09-26 02:18:54 +1300

    Display list of customers and books

--- BookRentals/Controllers/BooksController.cs
+++ BookRentals/Controllers/BooksController.cs
@@ -10,60 +10,81 @@ namespace BookRentals.Controllers
 {
     public class BooksController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetBooks();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book { Id = 1, Name = "Stock Trading for Beginners" },
+                new Book { Id = 2, Name = "The Drawing of the Three" }
+            };
+        }
+
+
+        // -----------------
         // GET: Books/Random
-        public ActionResult Random()
-        {
-            var book = new Book() { Name = "A Game of Thrones (Song of Ice and Fire)" };
+        //public ActionResult Random()
+        //{
+        //    var book = new Book() { Name = "A Game of Thrones (Song of Ice and Fire)" };
 
-            var customers = new List<Customer>
-            {
-                new Customer { Name ="Customer 1" },
-                new Customer { Name ="Customer 2" }
-            };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name ="Customer 1" },
+        //        new Customer { Name ="Customer 2" }
+        //    };
 
-            var viewModel = new RandomBookViewModel
-            {
-                Book = book,
-                Customers = customers
-            };
+        //    var viewModel = new RandomBookViewModel
+        //    {
+        //        Book = book,
+        //        Customers = customers
+        //    };
 
-            return View(viewModel);
+        //    return View(viewModel);
 
-            //return View(book);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home");
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        //    //return View(book);
+        //    //return Content("Hello World!");
+        //    //return HttpNotFound();
+        //    //return new EmptyResult();
+        //    //return RedirectToAction("Index", "Home");
+        //    //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
 
-            //ViewData["Book"] = book; // magic string - ViewData - ViewDataDictionary us as dictionary with key value pair or use it as model property to work with an object
-            //ViewBag.Book = book; // magic property - book added to the ViewBag.Book at runtime - no compile time safety
-            //return View();
-            //// dont use ViewData or ViewBag
-        }
+        //    //ViewData["Book"] = book; // magic string - ViewData - ViewDataDictionary us as dictionary with key value pair or use it as model property to work with an object
+        //    //ViewBag.Book = book; // magic property - book added to the ViewBag.Book at runtime - no compile time safety
+        //    //return View();
+        //    //// dont use ViewData or ViewBag
+        //}
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
 
-        // books 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        //// books 
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
 
-        [Route("books/published/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByPublishDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        //[Route("books/published/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByPublishDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+        // -----------------
+
     }
 }
---
+++ BookRentals/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookRentals.Models;
+
+namespace BookRentals.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        // Start- Entity Framework
+        //private ApplicationDbContext _context; // DbContext - disposable object
+
+        //public CustomerController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+
+        //protected override void Dispose(bool disposing) // override --> dispose --> tabtab
+        //{
+        //    _context.Dispose();
+        //}
+        // End - Entity Framework
+
+        // GET: Customer
+        public ViewResult Index()
+        {
+
+            var customers = GetCustomers();
+
+            //var customers = _context.Customers; // Deferred Execution
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+               new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
